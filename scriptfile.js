920dcad92f47dbdfd45c975c8bd85d1b2be70355
ayaznoori <ayaz.noori15@gmail.com>
2022-04-23 20:01:42 +0530

    Debouncing added

--- scriptfile.js
+++ scriptfile.js
@@ -1,3 +1,4 @@
+let id;
 async function trans(){
     const textinput=document.getElementById("inputtext").value;
     const inputopt=document.getElementById("inputopt").value;
@@ -15,12 +16,41 @@ async function trans(){
 });
 
    let result = await res.json();
-   console.log(result)
-   document.querySelector("#outputtext").value=result.translatedText;
+   return result.translatedText
+  /*  console.log(result)
+   document.querySelector("#outputtext").value=result.translatedText; */
     }
     catch(err){
         console.log(err)
     }
-      
+}
+    async function disp(data){
+        document.querySelector("#outputtext").value=data; 
+
+    }  
     
+
+async function main(){
+   let data = await trans();
+   if(data==undefined)
+   return false;
+   else
+    disp(data);
+}
+
+function debounce(func,delay){
+    if(id){
+        clearTimeout(id)
+    }
+     document.querySelector("#outputtext").value=".";
+     setTimeout(()=>{
+        document.querySelector("#outputtext").value="..";
+     },500)
+     setTimeout(()=>{
+        document.querySelector("#outputtext").value="...";
+     },1000)
+    id = setTimeout(() => {
+       func(); 
+    },delay);
+
 }
